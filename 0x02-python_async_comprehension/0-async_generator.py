db929500874c226edbfaafe13d748d934e04f570
samuel martins <samuel.martins4.sm@gmail.com>
2023-12-14 01:48:10 +0300

    chore: implement the async comprehension tasks

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
-
-"""Async Generator"""
-
+""" Async generator """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator() -> float:
-    """Async Generator"""
-    for i in range(10):
+async def async_generator() -> Generator[float, None, None]:
+    """ loop 10 times and in each asynchronously wait 1 second
+        then yield a random number between 0 and 10
+    """
+    for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random() * 10
